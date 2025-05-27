1d5aec4ccc2a68b805d66cebba6c13a0de383609
Rodolfo Kong <kongrodolfo@gmail.com>
2025-05-27 15:34:07 -0600

    Update __init__.py

--- src/validators/__init__.py
+++ src/validators/__init__.py
@@ -2,7 +2,9 @@
 FEL Validators Package
 """
 from .xml_validator import XMLValidator, SchemaManager, ValidationResult, create_xml_validator
+from .business_validator import BusinessValidator, BusinessValidationResult, create_business_validator
 
 __all__ = [
-    'XMLValidator', 'SchemaManager', 'ValidationResult', 'create_xml_validator'
+    'XMLValidator', 'SchemaManager', 'ValidationResult', 'create_xml_validator',
+    'BusinessValidator', 'BusinessValidationResult', 'create_business_validator'
 ]
